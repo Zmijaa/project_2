6afc1fe44dd9f6512be3051c51387f7dc18ace4f
Filip Mircev <147530688+Zmijaa@users.noreply.github.com>
2023-10-14 18:00:26 +0200

    Add files via upload

---
+++ project_2/src/Menuvacnica.java
@@ -0,0 +1,13 @@
+
+public class Menuvacnica {
+
+	public static void main(String[] args) {
+int denari=340;
+double evro=61.7;
+double x=denari/evro;
+System.out.println("evravodenari");
+System.out.println(x);
+
+	}
+
+}
